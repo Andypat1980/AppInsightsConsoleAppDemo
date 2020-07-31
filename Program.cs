029354d6d03abf60acd4de377ca8b939d4b61ed8
Andy <andypat1980@gmail.com>
2020-07-30 19:06:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppInsightsConsoleAppDemo
+{
+    class Program
+    {
+        public static object CurrentTime { get; private set; }
+
+        static void Main(string[] args)
+        {
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+
+            configuration.InstrumentationKey = "a304ec24-e939-45ca-9aa6-1363b4551aab";
+            configuration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+
+            var telemetryClient = new TelemetryClient(configuration);
+            using (InitializeDependencyTracking(configuration))
+            {
+                // run app...
+                String timeStamp = GetTimestamp(DateTime.Now);
+                telemetryClient.TrackTrace("Hello World-"+ timeStamp);
+            }
+
+            // before exit, flush the remaining data
+            telemetryClient.Flush();
+
+            // flush is not blocking when not using InMemoryChannel so wait a bit. There is an active issue regarding the need for `Sleep`/`Delay`
+            // which is tracked here: https://github.com/microsoft/ApplicationInsights-dotnet/issues/407
+            Task.Delay(5000).Wait();
+        }
+        static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyyMMddHHmmssffff");
+        }
+
+        static DependencyTrackingTelemetryModule InitializeDependencyTracking(TelemetryConfiguration configuration)
+        {
+            var module = new DependencyTrackingTelemetryModule();
+
+            // prevent Correlation Id to be sent to certain endpoints. You may add other domains as needed.
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.chinacloudapi.cn");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.cloudapi.de");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.usgovcloudapi.net");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("localhost");
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("127.0.0.1");
+
+            // enable known dependency tracking, note that in future versions, we will extend this list. 
+            // please check default settings in https://github.com/microsoft/ApplicationInsights-dotnet-server/blob/develop/WEB/Src/DependencyCollector/DependencyCollector/ApplicationInsights.config.install.xdt
+
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.ServiceBus");
+            module.IncludeDiagnosticSourceActivities.Add("Microsoft.Azure.EventHubs");
+
+            // initialize the module
+            module.Initialize(configuration);
+
+            return module;
+        }
+    }
+}
